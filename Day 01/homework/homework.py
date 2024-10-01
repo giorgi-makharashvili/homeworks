d43e83e84e3f339f8182386d14435c42cbd273a7
giorgi-makharashvili <maxarashviligiorgi430@gmail.com>
2024-10-01 21:15:09 +0400

    commit

---
+++ Day 01/homework/homework.py
@@ -0,0 +1,8 @@
+name = "giorgi"
+surname = "makharashvili"
+age = 16
+height = 176.5
+favorite_color = "black"
+loves_goa = True
+
+print(name + " " + surname + " " + str(age) + " " + str(height) + " " + str(favorite_color) + " " + str(loves_goa))
