ff0e484a2f71225ec7add0027e82d8d1dd303547
giorgi-makharashvili <maxarashviligiorgi430@gmail.com>
2024-10-01 21:02:47 +0400

    commit

--- Day 01/classwork/classwork.py
+++ Day 01/classwork/classwork.py
@@ -1,1 +1,21 @@
-#no classwork
+name = "giorgi"
+# name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "giorgi" არის ცვლადისთვის მნიშვნელობა
+surname = "makharashvili"
+age = 16
+height= 176.5
+
+
+#print(name)
+#print ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "giorgi" # ეს არის str (string) ტიპის ცვლადი
+age = 16  # ეს არის int (integer) მთელი რიცხვი
+height =176.5 # ეს არის float ტიპის ცვლადი (ათწილადი)
+Knows_programing = True   # True ან False # ეს არის bool (boolean) ტიპის ცვლადი
+is_ugly = False #snakecase (უბრალოდ წერის სტილი სტანდარტულად)
+
+isUgly = False #camelcase (ჯავასკრიპტული)
+
+print(name + " " + str(age) ) 
