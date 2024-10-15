bab60b2e5b8a22ae0dc5ca6dd485b203932c8288
giorgi-makharashvili <maxarashviligiorgi430@gmail.com>
2024-10-15 22:58:12 +0400

    commit

---
+++ Day 04/classwork/classwork.py
@@ -0,0 +1,54 @@
+#1)
+name = "giorgi"
+surname = "makharashvili"
+num = 20
+num2 = 10
+num3 = 12.5
+num4 = 10.3
+
+print(name)
+print(surname)
+print(num)
+print(num2)
+print(num3)
+print(num4)
+
+#2)
+print(20 + 10)
+print(5 + 3)
+print(6 + 3)
+print(8 + 4)
+print(9 + 5)
+
+print(20 - 10)
+print(60 -7)
+print(20 -5)
+print(12 -5)
+print(10 -5)
+
+print(20 * 10)
+print(2 * 7)
+print(3 * 9)
+print(4 *30)
+print(6 * 10)
+
+print(20 / 10)
+print(50 /40)
+print(10 /2)
+print(70 / 10)
+print(20 / 10)
+
+print(20 % 6)
+print(12 % 2)
+print(43 % 5)
+print(20 % 3)
+print(44 % 3)
+
+print(5 ** 4)
+print(5 ** 3)
+print(3 ** 7)
+print(2 ** 9)
+print(1 ** 9)
+
+#3)
+print((2 + 3) * (4 - 2) / (2 ** 4) * (20 % 6) )
