e71ae5fd7abf6023b35e43b82da8d3de44fd1a3c
giorgi-makharashvili <maxarashviligiorgi430@gmail.com>
2024-10-26 15:58:26 +0400

    commit

---
+++ Day 6/classwork/classwork.py
@@ -0,0 +1,99 @@
+num = 10
+num += 20 
+num4 = 30
+num4 +=50
+num5 =14
+num5 +=10
+num6 = 12
+num6 += 11
+num7 = 13
+num7 += 11
+
+num1 = 25
+num1 -=  10
+num8 = 10
+num8 -=3
+num9 = 40
+num9 -=20
+num10 = 50
+num10 -= 30
+num11 = 70
+num11-=30
+
+num2 = 30
+num2 *=  2
+num12 = 6
+num12 *=5
+num13=9
+num13*=7
+num14=8
+num14*=4
+num15=3
+num15*=5
+
+num3 = 40
+num3 /= 2
+num16 = 10
+num16 /=5
+num17 = 6
+num17 /= 8
+num18 =9
+num18 /=6
+num19 =4
+num19 /=5
+
+print(num)
+print(num1)
+print(num2)
+print(num3)
+print(num4)
+print(num5)
+print(num6)
+print(num7)
+print(num8)
+print(num9)
+print(num10)
+print(num11)
+print(num12)
+print(num13)
+print(num14)
+print(num15)
+print(num16)
+print(num17)
+print(num18)
+print(num19)
+
+print ("Hello World")
+variable = 6
+print(variable)
+print("Hello World")
+print("This is an indented line")
+undeclared_variable = 7
+print(undeclared_variable)
+print("Hello World")
+x = 10
+y = 5
+print(x + y)
+print("Missing indentation in function")
+var = 10
+print(var)
+ab = 10
+print(ab)
+print("Unclosed string")
+print("Escaped quote:")
+# Missing comment symbol
+#This is a comment
+print("No colon in if statement")
+variable = 55
+print("Empty statement after variable declaration")
+
+
+
+
+
+
+
+
+
+
+
