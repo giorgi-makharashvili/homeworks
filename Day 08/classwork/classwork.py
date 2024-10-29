20d5c9d84337251327aa2378b798d6112846d8a1
giorgi-makharashvili <maxarashviligiorgi430@gmail.com>
2024-10-29 22:55:14 +0400

    commit

---
+++ Day 08/classwork/classwork.py
@@ -0,0 +1,36 @@
+#1)
+name=input("enter your name: ")
+print(name)
+surname=input("enter your surname: ")
+print(surname)
+age=input("enter your age")
+print(age)
+num=input("enter your number")
+print(num)
+num1=input("enter your number")
+print(num1)
+#2)
+num=input("enter your number: ")
+num1=input("enter your number: ")
+print(num + num1)
+
+#3)
+name=input("enter your name: ")
+print(type(name))
+surname=input("enter your surname: ")
+print(type(surname))
+age=input("enter your age")
+print(type(age))
+num=input("enter your number")
+print(type(num))
+num1=input("enter your number")
+print(type(num1))
+
+
+
+
+
+
+
+
+
