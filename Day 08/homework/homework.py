a26832143965eac6e40358f6ab4175f422558892
giorgi-makharashvili <maxarashviligiorgi430@gmail.com>
2024-11-03 21:43:34 +0400

    commit

---
+++ Day 08/homework/homework.py
@@ -0,0 +1,21 @@
+#4)
+x = input("enter your name: ")
+y = input("enter your surname: ")
+print("Hello" + " " + x + " " + y)
+
+#5)
+num1 = 1
+num2 = 2
+num3 = 3
+num4 = 4
+num5 = 5
+print((num5 % (num1 + num2) / (num3 * num1) - num4)**num1)
+print((num5 + (num1 - num2) % (num3 * num1) ** num4)*num1)
+print(num5 / (num1 + num2) * (num3 % num1) + num4**num1)
+print(((num1 + num2) - (num3 * num4) -num5 % num1) / num2)
+print(num4 / (num3 + num5) * (num1 % num2) + num3**num5)
+print(((num1 - num5) - (num2 * num4) / num5 % num1) - num2)
+print((num4 / (num3 -num1) / (num5 * num2) - num5)**num1)
+print(num4 / (num2 + num3) * (num4 % num5) + num1**num2)
+print(((num4 + num1) - (num2 * num3) -num4 % num5) / num1)
+print((num4 % (num3 + num2) / (num5 * num1) - num3)**num2)
