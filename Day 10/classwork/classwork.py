922cc97c80b6f5f06b0f5437f8afcfc8cc33d30d
giorgi-makharashvili <maxarashviligiorgi430@gmail.com>
2024-11-06 00:31:51 +0400

    commit

---
+++ Day 10/classwork/classwork.py
@@ -0,0 +1,55 @@
+#1)
+num = int(input("enter your number: "))
+print(num)
+num1 = float(input("enter your number2: "))
+print(num1)
+num2 = int(input("enter your age: "))
+print(num2)
+
+
+#2)
+weight = float(input("enter your weight: "))
+height = float(input("enter your height: "))
+bmi = weight / height**2
+print(bmi)
+
+#3)
+print(10>5)
+print(10>4)
+print(20>10)
+print(30>20)
+print(40>10)
+
+print(5<10)
+print(4<10)
+print(10<20)
+print(20<30)
+print(10<40)
+
+print(10>=50)
+print(50>=10)
+print(4>=5)
+print(10>=10)
+print(38>=10)
+
+print(4<=6)
+print(5<=5)
+print(6<=6)
+print(8<=9)
+print(9<=3)
+
+print(1==1)
+print(2==2)
+print(3==4)
+print(6==7)
+print(8==9)
+
+print(1!=2)
+print(2!=3)
+print(4!=5)
+print(6!=7)
+print(8!=8)
+
+
+
+
