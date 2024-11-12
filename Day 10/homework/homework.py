d99a2b0fcb9f6a1e3d2018f8ebef2397bcda0520
giorgi-makharashvili <maxarashviligiorgi430@gmail.com>
2024-11-12 18:27:59 +0400

    commit

--- Day 10/homework/homework.py
+++ Day 10/homework/homework.py
@@ -1,1 +1,291 @@
+#2)
+num= str(1)
+num1= str(2)
+num2= str(3)
+num3= str(4)
+num4= str(5)
+num5=str(6)
+num6=str(7)
+num7=str(8)
+num8=str(9)
+num9= str(10)
+print(num + num9)
+print(num2 + num3)
+print(num8 + num5)
+print(num7 + num8)
+print(num6 + num3)
+print(num9 + num6)
+print(num5 + num1)
+print(num + num2)
+print(num7 + num2)
+print(num1 + num3)
+
+number= float(1)
+number1= float(2)
+number2= float(3)
+number3= float(4)
+number4= float(5)
+number5=float(6)
+number6=float(7)
+number7=float(8)
+number8=float(9)
+number9= float(10)
+print(number + number1)
+print(number2 + number3)
+print(number4 + number5)
+print(number6 + number7)
+print(number8 + number9)
+print(number9 + number6)
+print(number5 + number3)
+print(number + number2)
+print(number7 + number2)
+print(number1 + number3)
+
+numb= int(1)
+numb1= int(2)
+numb2= int(3)
+numb3= int(4)
+numb4= int(5)
+numb5=int(6)
+numb6=int(7)
+numb7=int(8)
+numb8=int(9)
+numb9= int(10)
+print(numb + numb1)
+print(numb2 + numb3)
+print(numb4 + numb5)
+print(numb7 + numb8)
+print(numb5 + numb5)
+print(numb9 + numb6)
+print(numb9 + numb3)
+print(numb + numb2)
+print(numb7 + numb2)
+print(numb8 + numb3)
 #5) 
+c = 10
+c1 = 18
+c2 = 12
+c3 = 14
+c4 = 16
+c5 = 11
+c6 = 12
+c7 = 15
+c8 = 1
+c9 = 13
+c10 = 80
+c11 = 70
+c12 = 30
+c13 = 8
+c14 = 40
+c15 = 9
+c16 = 20
+c17 = 23
+c18 = 22
+c19 = 3
+print(c>c1)
+print(c2>c3)
+print(c4>c5)
+print(c6>c7)
+print(c8>c9)
+print(c10>c11)
+print(c12>c13)
+print(c14>c15)
+print(c16>c17)
+print(c18>c19)
+
+
+y = 10
+y1 = 18
+y2 = 12
+y3 = 14
+y4 = 16
+y5 = 11
+y6 = 12
+y7 = 15
+y8 = 14
+y9 = 13
+y10 = 80
+y11 = 70
+y12 = 60
+y13 = 50
+y14 = 40
+y15 = 30
+y16 = 20
+y17 = 23
+y18 = 22
+y19 = 21
+print(y<y1)
+print(y2<y3)
+print(y4<y5)
+print(y6<y7)
+print(y8<y9)
+print(y10<y11)
+print(y12<y13)
+print(y14<y15)
+print(y16<y17)
+print(y18<y19)
+
+w = 10
+w1 = 18
+w2 = 12
+w3 = 14
+w4 = 16
+w5 = 11
+w6 = 12
+w7 = 15
+w8 = 14
+w9 = 13
+w10 = 8
+w11 = 7
+w12 = 6
+w13 = 5
+w14 = 4
+w15 = 3
+w16 = 2
+w17 = 23
+w18 = 22
+w19 = 21
+print(w<=w1)
+print(w2<=w3)
+print(w4<=w5)
+print(w6<=w7)
+print(w8<=w9)
+print(w10<=w11)
+print(w12<=w13)
+print(w14<=w15)
+print(w16<=w17)
+print(w18<=w19)
+
+v = 10
+v1 = 18
+v2 = 12
+v3 = 14
+v4 = 16
+v5 = 11
+v6 = 12
+v7 = 15
+v8 = 14
+v9 = 13
+v10 = 80
+v11 = 70
+v12 = 60
+v13 = 50
+v14 = 40
+v15 = 30
+v16 = 20
+v17 = 23
+v18 = 22
+v19 = 21
+print(v>=v1)
+print(v2>=v3)
+print(v4>=v5)
+print(v6>=v7)
+print(v8>=v9)
+print(v10>=v11)
+print(v12>=v13)
+print(v14>=v15)
+print(v16>=v17)
+print(v18>=v19)
+
+l = 10
+l1 = 18
+l2 = 12
+l3 = 14
+l4 = 16
+l5 = 11
+l6 = 12
+l7 = 15
+l8 = 14
+l9 = 13
+l10 = 8
+l11 = 7
+l12 = 6
+l13 = 5
+l14 = 4
+l15 = 3
+l16 = 2
+l17 = 23
+l18 = 22
+l19 = 21
+print(l==l1)
+print(l2==l3)
+print(l4==l5)
+print(l6==l7)
+print(l8==l9)
+print(l10==l11)
+print(l12==l13)
+print(l14==l15)
+print(l16==l17)
+print(l18==l19)
+
+m = 10
+m1 = 186
+m2 = 12
+m3 = 14
+m4 = 16
+m5 = 11
+m6 = 12
+m7 = 15
+m8 = 14
+m9 = 13
+m10 = 8
+m11 = 7
+m12 = 6
+m13 = 5
+m14 = 4
+m15 = 3
+m16 = 2
+m17 = 2
+m18 = 207
+m19 = 21
+print(m!=m1)
+print(m2!=m3)
+print(m4!=m5)
+print(m6!=m7)
+print(m8!=m9)
+print(m10!=m11)
+print(m12!=m13)
+print(m14!=m15)
+print(m16!=m17)
+print(m18!=m19)
+
+#6)
+age = int(input(" enter your age: "))
+age2 = age + 10
+print( age2)
+
+#7)
+nums1 = int(input("enter your age: "))
+nums2= int(input("enter your age: "))
+nums3= int(input("enter your age: "))
+average = (nums1 + nums2 + nums3)/3
+print(average)
+
+#8)
+x1 = float(input("enter your number: "))
+x2 = float(input("enter your second number: "))
+a1 = x1 + x2
+a2 = x1 - x2
+a3 = x1 * x2
+a4 = x1 / x2
+a5 = x1 ** x2
+a6 = x2 ** x1
+print(a1)
+print(a2)
+print(a3)
+print(a4)
+print(a5)
+print(a6)
+
+#9)
+b1 = float(input("enter your 1st number: "))
+b2 = float(input("enter your 2nd number: "))
+b3 = float(input("enter your 3rd number: "))
+j1 = b1 + b2 + b3
+j2 = b1 - b2 - b3
+j3 = b1 * b2 * b3
+j4= b1 / b2 / b3
+print(y1)
+print(j2)
+print(j3)
+print(j4)
