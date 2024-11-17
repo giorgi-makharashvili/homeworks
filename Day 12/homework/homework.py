59cf18d25951d49242415a89e12311db529767e8
giorgi-makharashvili <maxarashviligiorgi430@gmail.com>
2024-11-17 17:27:08 +0400

    commit

---
+++ Day 12/homework/homework.py
@@ -0,0 +1,51 @@
+#2)
+print((False and True) or True)
+print((True and True) or False)
+
+#3)
+x = 3 < 5
+print((True and x) and 7>2)
+
+x1 = True or False
+print((4<5) and (x1))
+
+x2 = False and False
+x3 = True and False
+print((x2 or x1) and 7>5)
+
+x4 = False or True
+x5 = 20 > 2
+print(x4 and x5)
+
+x6 = 50 > 60
+x7 = False
+print(x6 and x7)
+
+x8 = False and True and False
+print(True or x8)
+
+x9 = False and True
+x10 = 1 > 2
+print((x9 or x10) and True)
+
+x11 = True
+x12 = 40 < 100
+print((x11 and x12) or 5<23)
+
+x13 = True and True and False
+print(x13 or True)
+
+x14 = 50 > 3
+x15 = True
+print((x14 or x15) or True)
+
+#4) ნაჩვენებია ნახაზზე.
+
+#5) control flow არის მიმდევრობა რომლითაც ინდივიდუალური განცხადებები, ინსტრუქციები შესრულდება.
+# control flow იყოფა სამ ჯგუფად : sequences(თანმიმდევრობა), iterations(განმეორება), selections(სელექცია).
+
+#6) ნაჩვენებია ნახაზზე.
+
+#7)
+#ალგორითმები იძლევა ინსტრუქციების დეტალურ კომპლექტს, რომლითაც შეგიძლიათ მონაცემების დამუშავება კონკრეტული მიზნით.
+#ალგორითმების წარმოდგენის გზებია: natural language, pseudocode, flowchart, programming language.
