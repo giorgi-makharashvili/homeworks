49a76c08701ce038346203e8e8dc905f60b0172f
giorgi-makharashvili <maxarashviligiorgi430@gmail.com>
2024-11-19 22:53:42 +0400

    commit

---
+++ Day 14/classwork/classwork.py
@@ -0,0 +1,21 @@
+
+
+for i in range(20):
+    print("giorgi makharashvili")
+
+
+num = int(input("enter your number: "))
+num1 = int(input("enter your 2nd number: "))
+for i in range(num,num1):
+    print(i**2)
+    print(i + num)
+    print(i + num1)
+    print(i * num)
+    print(i * num1)
+
+
+n = int(input("enter your number n: "))
+for i in range(1,n):
+    print(i**2)
+
+
