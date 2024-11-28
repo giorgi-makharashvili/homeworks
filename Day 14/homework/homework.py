f1ecbacaa24f602fd76ba675c17da37bbdd79841
giorgi-makharashvili <maxarashviligiorgi430@gmail.com>
2024-11-28 23:13:06 +0400

    commit

---
+++ Day 14/homework/homework.py
@@ -0,0 +1,53 @@
+#2ციკლი და იტერაცია ნიშნავს რაღაცის განმეორებას.
+
+#3 ნახაზზეა ნაჩვენები.
+
+#4
+a = range(20,50)
+
+#5
+b = range(100,150)
+
+#6
+c = range(20,30,2)
+
+#7
+d = range(11,31,3)
+
+#8
+for i in range(10):
+    print("giorgi makharashvili")
+
+#9
+for i in range(10,30):
+    print(i/2)
+
+#10
+for i in range(40,60):
+    print(i**3)
+
+#11
+for i in range(5):
+    user_num = float(input("Enter your number: "))
+    print(user_num)
+
+#12
+name = input("Enter your name: ")
+for i in name:
+    print(i)
+
+#13
+num1 = int(input("Enter your number: "))
+num2 = int(input("Enter your number: "))
+num3 = int(input("Enter your number: "))
+range1 = range(num1, num2, num3)
+for i in range1:
+    print(i**2)
+
+#14
+sum = 0
+for i in range(5,16):
+    sum += i
+print(sum)
+
+#15 საკმაოდ.
---
+++ Day 16/classwork/classwork.py
@@ -0,0 +1,25 @@
+#1)
+num = int(input("enter your number: "))
+sum = 0
+
+for i in range(num):
+    sum = sum + i
+print(sum)
+
+#2)
+
+
+#3)
+counter = 0
+my_num = 5
+x = int(input("enter number: "))
+while x != my_num:
+    x = int(input("enter number: "))
+    counter = counter + 1
+print("guessed correct")
+print(counter)
+
+
+
+
+
