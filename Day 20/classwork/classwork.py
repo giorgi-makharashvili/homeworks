d323c18ec8ecc3edea2f99d24161a294c4b9984e
giorgi-makharashvili <maxarashviligiorgi430@gmail.com>
2024-12-10 22:50:31 +0400

    commit

--- Day 20/classwork/classwork.py
+++ Day 20/classwork/classwork.py
@@ -1,3 +1,22 @@
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+a = list[1]
+a1 = list[2]
+a2 = list[3]
+a3 = list[4]
+a4 = list[5]
+a5 = list[6]
+a6 = list[7]
+a7 = list[8]
+a8 = list[9]
+a9 = list[10]
+
+
+num = int(input("enter number"))
+num1 = int(input("enter number"))
+num2 = int(input("enter number"))
+list2 = [num, num1, num2]
+print(list2)
+
 
 
 
