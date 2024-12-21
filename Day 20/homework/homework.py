9fc561a6dfc856c9d1162023328b12d855d47ffc
giorgi-makharashvili <maxarashviligiorgi430@gmail.com>
2024-12-21 18:58:18 +0400

    commit

--- Day 20/homework/homework.py
+++ Day 20/homework/homework.py
@@ -36,4 +36,51 @@ else:
     print("is 0")
 
 #6)
+num6 = int(input("enter your 1 number: "))
+num7 = int(input("enter your 2 number: "))
+g = 0
+for i in range(num6,num7+1):
+    g+=i
+print(g)
+#7)
+num8 = int(input("Enter your number: "))
+is_valid = True
 
+if num8 < 0:
+    is_valid = False
+for i in range(2, num1):
+    if num8 % i == 0:
+        is_valid = False
+if is_valid == True:
+    print("Your number is prime")
+else:
+    print("Your number is not prime")
+
+#8)
+list5 = [1, 2, 3, 4, 5]
+print(list5[0])
+print(list5[2])
+print(list5[4])
+
+#9)
+list6 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+print(list6[0])
+print(list6[1])
+print(list6[2])
+print(list6[3])
+print(list6[4])
+print(list6[5])
+print(list6[6])
+print(list6[7])
+print(list6[8])
+print(list6[9])
+print(list6[10])
+print(list6[11])
+print(list6[12])
+print(list6[13])
+print(list6[14])
+print(list6[15])
+print(list6[16])
+print(list6[17])
+print(list6[18])
+print(list6[19])
