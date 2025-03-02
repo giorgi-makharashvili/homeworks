2af622a84d0af09fb81101f311368d123add3347
giorgi-makharashvili <maxarashviligiorgi430@gmail.com>
2025-03-02 21:50:20 +0400

    commit

---
+++ Day 59/classwork/script.js
@@ -0,0 +1,22 @@
+// 1)
+let num1 = Number(prompt("enter number"));
+let num2 = Number(prompt("enter number2"));
+
+console.log(num1+num2)
+
+//2)
+
+let name1 = prompt("enter your name");
+
+console.log("Hello, " + name1)
+
+//3)
+
+
+
+
+
+
+
+
+
