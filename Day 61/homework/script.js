d2c97b272e647089f130edb36f62522e96d0dd75
giorgi-makharashvili <maxarashviligiorgi430@gmail.com>
2025-03-11 22:55:43 +0400

    commit

---
+++ Day 61/homework/script.js
@@ -0,0 +1,31 @@
+//2)
+console.log(5 != "5");   
+console.log(10 != 20);   
+console.log(0 != 10); 
+console.log(20 != 13);
+console.log(5 + 10)
+console.log(10 - 20)
+console.log(12 * 2)
+console.log(4 / 2)
+console.log(20 - 10)
+console.log(10 < 5);   
+console.log(5 < 10);   
+console.log(5 >= 5);  
+console.log(10 <= 5);  
+
+
+
+
+//3)
+// alert' საიტზე შესვლისას გაფრთხილებას აძლევს მომხმარებელს 
+
+// pormpt'ის მეშვეობით მომხარებელს შემოაქვს ინფორმაცია
+
+// confrim  ფუნქციას  გამოაქვს ბოქსი OK და Cancel ღილაკებით.
+
+//4)
+if (confirm("Are you an adult?")) {
+    alert("You are adult");
+} else {
+    alert("You are kid");
+}
