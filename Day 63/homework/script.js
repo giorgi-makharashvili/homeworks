c4fbbbe7d58a757aeba213ba28edc8ac5c5fcd2e
giorgi-makharashvili <maxarashviligiorgi430@gmail.com>
2025-03-20 00:52:44 +0400

    commit

---
+++ Day 63/homework/script.js
@@ -0,0 +1,29 @@
+// 2)
+let i = 1;
+
+while (i<200) {
+    console.log(i);
+    i += 2;
+}
+
+// 3)
+
+let x = 51;
+
+while (x<=150) {
+    console.log(x);
+    x +=3;
+}
+
+// 4)
+
+for (let i = 0; i <101; i++) {
+    console.log(i);
+}
+
+// 5)
+
+for (let i = 100; i<301; i+=2) {
+    console.log(i);
+}
+    
